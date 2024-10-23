f41e7cedb4b8cdf43647b74590f6989f9aab490c
Lyu80V <7222707@gmail.com>
2024-10-23 14:38:40 +0300

    Add files via upload

---
+++ module_10_1.py
@@ -0,0 +1,39 @@
+
+from time import sleep
+from threading import Thread
+from datetime import datetime
+
+def write_words(word_count, file_name):
+    with open(file_name, 'w', encoding='utf-8') as file:
+        for i in range(word_count):
+            file.write(f'Какое-то слово № {i + 1}\n')
+            sleep(0.1)
+        print(f'Завершилась запись в файл {file_name}')
+start = datetime.now()
+
+write_words(10, 'example1.txt')
+write_words(30, 'example2.txt')
+write_words(200, 'example3.txt')
+write_words(100, 'example4.txt')
+end = datetime.now()
+print(f'Работа функции составила {end - start}')
+
+start = datetime.now()
+
+th1 = Thread(target=write_words, args=(10, 'example5.txt'))
+th2 = Thread(target=write_words, args=(30, 'example6.txt'))
+th3 = Thread(target=write_words, args=(200, 'example7.txt'))
+th4 = Thread(target=write_words, args=(100, 'example8.txt'))
+
+th1.start()
+th2.start()
+th3.start()
+th4.start()
+
+th1.join()
+th2.join()
+th3.join()
+th4.join()
+
+finish = datetime.now()
+print(f'Работа потоков {finish - start}')
